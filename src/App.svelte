f0577a81fa2df49064b101d46cd111e12c794986
Maya084 <37115588+Maya084@users.noreply.github.com>
2023-09-11 17:12:39 +0200

    fix style; add alert for invalid json

--- src/App.svelte
+++ src/App.svelte
@@ -1,33 +1,34 @@
-<script>
-  let imageUrlJSON = '';
-  let imageUrls = [];
+<script lang="ts">
+  let imageUrlJSON:string  = "";
+  let imageUrls: string[] = [];
 
   function visualizeImages() {
     try {
       imageUrls = JSON.parse(imageUrlJSON);
     } catch (error) {
-      console.error('Invalid JSON format:', error);
+      alert(`Invalid JSON format: ${error}`);
       imageUrls = [];
     }
   }
-
 </script>
+<main>
 
 <div class="json-input">
   <textarea
     bind:value={imageUrlJSON}
     placeholder="Enter JSON list of image URLs"
-  ></textarea>
+  />
   <button on:click={visualizeImages}>Visualize Images</button>
 </div>
 
 <div class="custom-grid">
   {#each imageUrls as url (url)}
     <article>
-      <img src={url} alt="Image" loading="lazy"/>
+      <img src={url} alt="Image" loading="lazy" />
     </article>
   {/each}
 </div>
+</main>
 
 <style>
   .custom-grid {
@@ -38,5 +39,8 @@
   main {
     padding: 1em;
   }
-</style>
 
+  article{
+    height: fit-content;
+  }
+</style>
